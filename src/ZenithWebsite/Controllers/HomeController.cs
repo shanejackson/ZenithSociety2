041b1eb43737df27b8dd18318c39ef3ff1cc5390
shanejackson <sjacksondev@gmail.com>
2017-03-25 16:19:03 -0700

    trying to display events on homescreen

--- src/ZenithWebsite/Controllers/HomeController.cs
+++ src/ZenithWebsite/Controllers/HomeController.cs
@@ -3,30 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ZenithWebsite.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ZenithWebsite.Controllers
 {
     public class HomeController : Controller
     {
+        private const string FORMAT = "MMMM dd, yyyy";
+        private ZenithContext db;
+
+        public HomeController(ZenithContext context)
+        {
+            db = context;
+        }
+
         public IActionResult Index()
         {
+            var @event = db.Events.Include(that => that.Activity);
+
+            Dictionary<String, List<Models.Event>> Week = new Dictionary<String, List<Event>>();
+
+            //Find the monday of this week
+            DateTime today = DateTime.Now;
+            int range = DayOfWeek.Monday - today.DayOfWeek;
+            if (range > 0)
+                range -= 7; // Always get this weeks dates
+            DateTime monday = today.Date.AddDays(range);
+            DateTime nextMonday = monday.AddDays(7);
+            ViewBag.StartOfWeek = monday.ToString(FORMAT);
+
+            //Allow only days this week
+            var daysOfTheWeek = @event.Where(e => e.EventFrom >= monday && e.EventTo < nextMonday);
+
+            //add to dictionary
+            foreach (var e in daysOfTheWeek.OrderBy(name => name.EventFrom).ToList())
+            {
+                if (e.IsActive)
+                {
+                    if (Week.ContainsKey(e.EventFrom.ToString(FORMAT)))
+                    {
+
+                        Week[e.EventFrom.ToString(FORMAT)].Add(e);
+                    }
+                    else
+                    {
+                        Week[e.EventFrom.ToString(FORMAT)] = new List<Event> { e };
+                    }
+                }
+            }
+
+            ViewBag.Week = Week.ToList();
+
             return View();
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
+    
         public IActionResult Error()
         {
             return View();
